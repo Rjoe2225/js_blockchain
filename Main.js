68a8f1dbc9dd2ead0fd5749f79adba39c4a08b67
Rjoe2225 <rjoe5000@gmail.com>
2021-08-12 13:26:24 -0400

    proof of work update

--- Main.js
+++ Main.js
@@ -1,4 +1,4 @@
-const SHA256 = require('crypto-js/sha256')
+const SHA256 = require('crypto-js/sha256');
 
 
 class Block{
@@ -8,19 +8,27 @@ class Block{
 		this.data = data;
 		this.previousHash = previousHash;
 		this.hash = '';
+		this.nonce =0;
 	}
 
 
 	calculateHash(){
-		return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+		return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+	}
 
-
+	mineBlock(difficulty){
+		while(this.hash.substring(0,difficulty)!== Array(difficulty + 1).join("0")){
+			this.nonce++;
+			this.hash=this.calculateHash;
+		}
+		console.log("Block mined: "+this.hash);
 	}
 }
 
 class Blockchain{
 	constructor(){
 		this.chain =[this.creatGenesisBlock()];
+		this.difficulty = 2;
 	}
 
 	creatGenesisBlock(){
@@ -32,7 +40,7 @@ class Blockchain{
 
 	addBlock(newBlock){
 		newBlock.previousHash = this.getLatestBlock().hash;
-		newBlock.hash = newBlock.calculateHash();
+		newBlock.mineBlock(this.difficulty);
 		this.chain.push(newBlock);
 	}
 
@@ -54,15 +62,22 @@ class Blockchain{
 	}
 }
 
-let potatCoin = new Blockchain();
-potatCoin.addBlock(new Block(1,"09/11/2021",{amount:4}));
-potatCoin.addBlock(new Block(1,"09/11/2021",{amount:16}));
-
-console.log('Is Blockchain valid?' + ' '+potatCoin.isChainValid());
-
-potatCoin.chain[1].data = {amount: 100};
+let testCoin = new Blockchain();
 
 
-console.log('Is Blockchain valid?' + ' '+potatCoin.isChainValid());
+//console.log("Mining block 1...")
 
-//console.log(JSON.stringify(potatCoin,null,4));
+//testCoin.addBlock(new Block(1,"09/11/2021",{amount:4}));
+
+//console.log("Mining block 2...")
+
+//testCoin.addBlock(new Block(1,"09/11/2021",{amount:16}));
+
+//console.log('Is the Blockchain valid?' + ' '+potatCoin.isChainValid());
+
+//testCoin.chain[1].data = {amount: 100};
+
+
+//console.log('Is the Blockchain valid?' + ' '+testCoin.isChainValid());
+
+console.log(JSON.stringify(testCoin,null,4));
